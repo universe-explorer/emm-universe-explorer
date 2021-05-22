fe51f0e89f94737b8145b2fb01f3598490d2d469
Dong Yang <Dong.Yang@Student.HTW-Berlin.de>
2021-05-22 20:19:13 +0200

    add initial level system

---
+++ Assets/Scripts/levelSystem/LevelSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem
+{
+    private int level;
+    private int experience;
+    private int experienceToNextLevel;
+
+    public LevelSystem()
+    {
+        level = 0;
+        experience = 0;
+        experienceToNextLevel = 100;
+    }
+
+    public void AddExperience(int amount)
+    {
+        experience += amount;
+        while (experience >= experienceToNextLevel)
+        {
+            level++;
+            experience -= experienceToNextLevel;
+        }
+    }
+
+    public int GetLevelNumber()
+    {
+        return level;
+    }
+}
