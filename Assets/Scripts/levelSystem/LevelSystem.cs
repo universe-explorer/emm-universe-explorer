ee3730109b8d1a0e432c1168447da6e09340c859
Dong Yang <Dong.Yang@Student.HTW-Berlin.de>
2021-05-22 20:36:16 +0200

    add events to level system

--- Assets/Scripts/levelSystem/LevelSystem.cs
+++ Assets/Scripts/levelSystem/LevelSystem.cs
@@ -3,8 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * This level System should be encapsulated into game Object that implements Monobehaviour.
+ * 
+ * To enable the game object reacting to experience changes, subscribe to the OnExperienceChanged event.
+ *      
+ * To enable the game object reacting to level changes, subscribe to the OnLevelChanged event.
+ *      
+ */
 public class LevelSystem
 {
+    public event EventHandler OnExperienceChanged;
+    public event EventHandler OnLevelChanged;
+    
     private int level;
     private int experience;
     private int experienceToNextLevel;
@@ -23,7 +34,9 @@ public class LevelSystem
         {
             level++;
             experience -= experienceToNextLevel;
+            if (OnLevelChanged != null) OnLevelChanged(this, EventArgs.Empty);
         }
+        if (OnExperienceChanged != null) OnExperienceChanged(this, EventArgs.Empty);
     }
 
     public int GetLevelNumber()
