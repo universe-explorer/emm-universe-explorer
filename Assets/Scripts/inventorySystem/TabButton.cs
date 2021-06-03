ec281fe02f4f3a8434c07c531fb991627942bede
Dong Yang <Dong.Yang@Student.HTW-Berlin.de>
2021-06-03 14:50:17 +0200

    Add inventory menu and tabs for displaying items of inventory system

---
+++ Assets/Scripts/inventorySystem/TabButton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public TabGroup tabGroup;
+
+    public Image background;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Image>();
+        tabGroup.subscribe(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
