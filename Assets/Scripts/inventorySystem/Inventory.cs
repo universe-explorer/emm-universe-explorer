34fc386d6596ac37673daa41c70cfdc6454fdb1c
Dong Yang <Dong.Yang@Student.HTW-Berlin.de>
2021-06-05 23:55:39 +0200

    display collected items within inventory system

--- Assets/Scripts/inventorySystem/Inventory.cs
+++ Assets/Scripts/inventorySystem/Inventory.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory
 {
+    public event EventHandler OnItemListChanged;
+
     private List<Item> itemList;
 
     public Inventory()
@@ -14,6 +17,7 @@ public class Inventory
     public void AddItem(Item item)
     {
         itemList.Add(item);
+        OnItemListChanged?.Invoke(this, EventArgs.Empty);
         Debug.Log("Inventory item counter: " + itemList.Count);
     }
 
--- Assets/Scripts/inventorySystem/Ui_inventory.cs
+++ Assets/Scripts/inventorySystem/Ui_inventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ui_inventory : MonoBehaviour
 {
@@ -17,10 +18,39 @@ public class Ui_inventory : MonoBehaviour
     public void SetInventory(Inventory inventory)
     {
         this.inventory = inventory;
+        inventory.OnItemListChanged += Inventory_OnItemListChanged;
+        RefreshInventoryItems();
+    }
+
+    private void Inventory_OnItemListChanged(object sender, System.EventArgs e)
+    {
+        RefreshInventoryItems();
     }
 
     private void RefreshInventoryItems()
     {
+        foreach (Transform child in itemSlotContainer)
+        {
+            if (child == itemSlotTemplate) continue;
+            Destroy(child.gameObject);
+        }
+        int x = 0;
+        int y = 0;
+        float itemSlotCellSize = 60f;
+        foreach (Item item in inventory.GetItemList())
+        {
+            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+            itemSlotRectTransform.gameObject.SetActive(true);
+            itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
+            Image image = itemSlotRectTransform.Find("image").GetComponent<Image>();
+            image.sprite = item.GetSprite();
+            x++;
+            if (x > 5)
+            {
+                x = 0;
+                y++;
+            }
 
+        }
     }
 }
