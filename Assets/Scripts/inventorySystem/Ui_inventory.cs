e726b53f39c3a82b5ad9568d00b5f6eb15b124ec
Dong Yang <Dong.Yang@Student.HTW-Berlin.de>
2021-06-06 18:57:11 +0200

    Fix null refenrence: instantiate inventory item slot and template inside start method

--- Assets/Scripts/inventorySystem/Ui_inventory.cs
+++ Assets/Scripts/inventorySystem/Ui_inventory.cs
@@ -8,7 +8,7 @@ public class Ui_inventory : MonoBehaviour
     private Transform itemSlotContainer;
     private Transform itemSlotTemplate;
 
-    private void Awake()
+    private void Start()
     {
         itemSlotContainer = transform.Find("itemSlotContainer");
         itemSlotTemplate = itemSlotContainer.Find("itemSlotTemplate");
@@ -34,14 +34,10 @@ public class Ui_inventory : MonoBehaviour
             if (child == itemSlotTemplate) continue;
             Destroy(child.gameObject);
         }
-        int x = 0;
-        int y = 0;
-        float itemSlotCellSize = 60f;
         foreach (Item item in inventory.GetItemList())
         {
             RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
             itemSlotRectTransform.gameObject.SetActive(true);
-            itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
 
             SetImage(itemSlotRectTransform, item);
 
