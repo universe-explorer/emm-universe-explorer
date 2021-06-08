08e7391d70584578a1bbe6365cb0658892184a5e
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-06-08 19:22:43 +0200

    Implemented Random Movement

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    private const int movementDuration = 200;
+    private const float movementRadius = 50;
+    private const float movementSpeed = 0.1f;
+    private const float rotationSpeed = 0.01f;
 
     private bool chaseMode;
+    private int framesSinceMovementStart;
+    private Vector3 movementDirection;
+    private Vector3 home;
+
     // Start is called before the first frame update
     void Start()
     {
         chaseMode = false;
+        framesSinceMovementStart = movementDuration;
+        home = transform.position;
     }
 
     // Update is called once per frame
@@ -34,11 +44,55 @@ public class EnemyBehaviour : MonoBehaviour
 
     public void randomMove()
     {
-        //TODO
+        if(framesSinceMovementStart >= movementDuration)
+        {
+            //Generate new destination point
+            do
+            {
+                movementDirection = Random.insideUnitCircle.normalized;
+            } while (Physics.Raycast(transform.position, movementDirection, movementSpeed * movementDuration));
+
+            framesSinceMovementStart = 0;
+            
+        } else
+        {
+            //Continue movement
+
+            //Rotate towards target
+            transform.rotation = Quaternion.Lerp(transform.rotation , Quaternion.LookRotation(movementDirection, Vector3.up), rotationSpeed);
+
+            //Move if there is no danger ahead
+            if (!Physics.Raycast(transform.position, transform.forward, movementSpeed * 3))
+            {
+                transform.position += transform.forward * movementSpeed;
+            } else
+            {
+                framesSinceMovementStart = movementDuration;
+            }
+
+            framesSinceMovementStart++;
+
+            if (Vector3.Distance(transform.position, home) > movementRadius)
+            {
+                Quaternion save = transform.rotation;
+                transform.LookAt(home);
+                movementDirection = transform.forward;
+                transform.rotation = save;
+                framesSinceMovementStart = 0;
+            }
+
+        }
     }
 
     public void chase()
     {
-        //TODO
+        if (framesSinceMovementStart >= movementDuration)
+        {
+            //Generate new destination point
+        }
+        else
+        {
+            //Continue movement
+        }
     }
 }
