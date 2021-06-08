1049ea143915935bbc5fcbd0c4b9512d79082d85
profanta <karl@neukoelln-berlin.de>
2021-06-08 14:26:21 +0200

    added some new Soundfiles and worked on a rudimental atmosphere system in the test scene

---
+++ Assets/CameraDebugLookAtPlanetScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraDebugLookAtPlanetScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(_target);
+    }
+}
