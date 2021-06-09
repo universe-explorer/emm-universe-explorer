4dd2f060d7ed237faf5136a6b12ebe4299e36423
eaxm <30189577+eaxm@users.noreply.github.com>
2021-06-09 19:35:14 +0200

    Added game state manager

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private KeyCode pauseMenuKey = KeyCode.Escape;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseMenuKey))
+            ShowMenu(!pauseMenu.activeSelf);
+        
+    }
+    
+    public void ShowMenu(bool active)
+    {
+        pauseMenu.SetActive(active);
+        Time.timeScale = active ? 0 : 1;
+    }
+    
+    
+
+    
+}
