a33609009ca23322a2e0bb37cfdbcbdae9db866a
eaxm <30189577+eaxm@users.noreply.github.com>
2021-06-10 05:28:20 +0200

    Added delayed health bar effect

--- Assets/Scripts/HealthBarScript.cs
+++ Assets/Scripts/HealthBarScript.cs
@@ -8,27 +8,38 @@ using UnityEngine.UI;
 
 public class HealthBarScript : MonoBehaviour
 {
-    [SerializeField] private GameObject healthBar;
+    [SerializeField] private GameObject currentHealthBar;
+    [SerializeField] private GameObject delayedHealthBar;
 
     private Image imgCurrentHealth;
+    private Image imgDelayedHealth;
     private float maxHealth = 100;
     private float currentHealth = 100;
+    private float progress = 0;
+    [SerializeField] private float delayedHealthBarSpeed = 0.02f;
 
     // Start is called before the first frame update
     void Start()
     {
-        imgCurrentHealth = healthBar.GetComponent<Image>();
-       
+        imgCurrentHealth = currentHealthBar.GetComponent<Image>();
+        imgDelayedHealth = delayedHealthBar.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //imgCurrentHealth.fillAmount = imgCurrentHealth.fillAmount - 0.1f*Time.deltaTime;
         imgCurrentHealth.fillAmount = currentHealth / maxHealth;
+
+        if (imgDelayedHealth.fillAmount > imgCurrentHealth.fillAmount)
+        {
+            progress += Time.deltaTime;
+            imgDelayedHealth.fillAmount = Mathf.Lerp(imgDelayedHealth.fillAmount, imgCurrentHealth.fillAmount, progress*delayedHealthBarSpeed);
+        }
+
         Debug.Log(imgCurrentHealth.fillAmount);
         
-        if(Input.GetKeyDown(KeyCode.Space))
+        // TODO: Remove before branch gets merged
+        if (Input.GetKeyDown(KeyCode.Space))
             TakeDamage(5);
     }
 
@@ -37,6 +48,7 @@ public class HealthBarScript : MonoBehaviour
         if (currentHealth - damage >= 0)
         {
             currentHealth -= damage;
+            progress = 0;
         }
     }
 }
