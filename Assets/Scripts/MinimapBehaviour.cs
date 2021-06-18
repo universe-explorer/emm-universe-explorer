9f186b6ee225a26c900061e5b1f3f9c54a3a6d22
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-06-18 18:03:09 +0200

    Implemented position update function

--- Assets/Scripts/MinimapBehaviour.cs
+++ Assets/Scripts/MinimapBehaviour.cs
@@ -6,21 +6,27 @@ public class MinimapBehaviour : MonoBehaviour
 {
 
     public Transform spaceShip;
-    private float cameraHeight;
+    private float cameraHeight = 100;
+    private float referenceHeight;
 
     void Start()
     {
-        
+        referenceHeight = spaceShip.position.y;
     }
 
     void FixedUpdate()
     {
-        
+        UpdatePosition();
     }
 
     private void UpdatePosition()
     {
-		
+        Vector3 newPosition = spaceShip.transform.position;
+        newPosition.y = referenceHeight + cameraHeight;
+        transform.position = newPosition;
+
+        Quaternion newRotation = Quaternion.Euler(90, spaceShip.rotation.eulerAngles.y, spaceShip.rotation.eulerAngles.z);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, 0.5f);
 	}
 
     public float getCameraHeight()
@@ -33,6 +39,7 @@ public class MinimapBehaviour : MonoBehaviour
     {
         cameraHeight = newCameraHeight;
     }
+
     private void CreatePlanetIcon()
     {
         // test 
