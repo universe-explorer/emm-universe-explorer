a426fd4e2303766c0e39bbcd8715bcf6199d6d0c
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-06-18 16:54:37 +0200

    Created empty behaviour script

---
+++ Assets/Scripts/MinimapBehaviour.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapBehaviour : MonoBehaviour
+{
+
+    public Transform spaceShip;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
