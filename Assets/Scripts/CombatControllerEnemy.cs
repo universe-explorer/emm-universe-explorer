8afd2830fc84838881d38009d8c4e7a5efde3545
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-07-02 15:40:24 +0200

    Enemies drop random items on death

--- Assets/Scripts/CombatControllerEnemy.cs
+++ Assets/Scripts/CombatControllerEnemy.cs
@@ -6,6 +6,18 @@ public class CombatControllerEnemy : AbstractCombatController
 {
     public override void Die()
     {
+        //Spawns a random item on death
+        Item.ItemType itemType = (Item.ItemType)Random.Range(0, System.Enum.GetNames(typeof(Item.ItemType)).Length);
+        Item item = new Item();
+        item.itemType = itemType;
+        item.amount = Random.Range(1, 10);
+        item.healthPortion = Random.Range(5, 16);
+        item.mineralPortion = Random.Range(1, 6);
+        item.manaPortion = Random.Range(5, 36);
+        item.medkitPortion = Random.Range(1, 3);
+        ItemWorld itemWorld = ItemWorld.SpawnItemWorld(transform.position, item);
+        itemWorld.transform.SetParent(transform.parent);
+
         Destroy(gameObject);
     }
 
