ab3effd88ad3b7ffaaa845abb17d72c4f248ac63
eaxm <30189577+eaxm@users.noreply.github.com>
2021-07-06 16:43:02 +0200

    Move spaceship via microcontroller input

---
+++ Assets/Scripts/SerialCommunication/MainThreadObj.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace SerialCommunication
+{
+    
+    
+    // Find a better name
+    public class MainThreadObj : MonoBehaviour
+    {
+        private float velocity = float.MinValue;
+        [SerializeField] private SpaceshipControls sc;
+        
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            if (velocity != float.MinValue)
+            {
+                sc.Move(((velocity / 255f) * sc.getMaximumVelocity()) - sc.getMaximumVelocity()/2);
+            }
+        }
+
+        public float Velocity
+        {
+            get => velocity;
+            set => velocity = value;
+        }
+    }
+}
--- Assets/Scripts/SerialCommunication/SpeedReader.cs
+++ Assets/Scripts/SerialCommunication/SpeedReader.cs
@@ -4,10 +4,22 @@ namespace SerialCommunication
 {
     public class SpeedReader : IReader
     {
+
+        private MainThreadObj mto;
+
+        public SpeedReader()
+        {
+            mto = GameObject.Find("SerialCommunicationObject").GetComponent<MainThreadObj>();
+        }
         public int read(byte[] data)
         {
             Debug.Log("Speed: " + data[0]);
             
+            //Debug.Log(sc.velocity);
+            //sc.Move((data[0] / 255) * 30);
+            //sc.Move(sc.transform.forward, 5);
+            mto.Velocity = data[0];
+            Debug.Log("main thread velocity: " + mto.Velocity);
             return 1;
         }
     }
